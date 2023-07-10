ca37f4d03c5bb275492cd7687f019d8e6451a8aa
David Yoon <david@Davids-MacBook-Pro.local>
2023-07-11 00:47:53 +0900

    Initial setting

---
+++ src/main/java/com/aimage/AimageApplication.java
@@ -0,0 +1,13 @@
+package com.aimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aimage/AimageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
