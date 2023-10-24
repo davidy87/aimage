1c20e42d3d2c0c2301aa8b07e88fe6f224091bcc
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-10-24 22:02:52 +0900

    [#30]chore: 무중단 배포 설정 적용

---
+++ src/test/java/com/aimage/ProfileControllerTest.java
@@ -0,0 +1,33 @@
+package com.aimage;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class ProfileControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    @DisplayName("profile은 인증없이 호출된다.")
+    void callProfile() {
+        String expected = "test";
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/com/aimage/ProfileControllerUnitTest.java
@@ -0,0 +1,63 @@
+package com.aimage;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProfileControllerUnitTest {
+
+    @Test
+    @DisplayName("realProfile이 조회된다.")
+    void findRealProfile1() {
+        // given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+        env.addActiveProfile(expectedProfile);
+
+        ProfileController profileController = new ProfileController(env);
+
+        // when
+        String profile = profileController.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    @DisplayName("realProfile이 없으면 마지막이 조회된다.")
+    void findRealProfile2() {
+        // given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile("real-db");
+        env.addActiveProfile(expectedProfile);
+
+        ProfileController profileController = new ProfileController(env);
+
+        // when
+        String profile = profileController.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    @DisplayName("activeProfile이 없으면 default가 조회된다.")
+    void findRealProfile3() {
+        // given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController profileController = new ProfileController(env);
+
+        // when
+        String profile = profileController.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
